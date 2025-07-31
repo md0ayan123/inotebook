f891df5755861d3291b00087707200cec8346732
Ayan <md.ayan835@gmail.com>
2025-07-31 16:34:50 +0530

    some changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 
 const Signup = (props) => {
-  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
+  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", })
   let navigate = useNavigate();
  const handleSubmit = async (e) => {
   e.preventDefault();
@@ -23,7 +23,7 @@ const Signup = (props) => {
     if (json.success) {
       localStorage.setItem('token', json.authtoken); // or json.token if that's what backend sends
       props.showAlert("Account created successfully", "success");
-      navigate('/');
+      navigate('/login');
     } else {
       props.showAlert("Invalid credentials", "danger");
     }
@@ -40,7 +40,7 @@ const Signup = (props) => {
 
   return (
     <div>
-      <div className='container my-3 mx-5'>
+      <div className='container '>
         <h3 className='my-4'>Signup</h3>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
@@ -56,10 +56,10 @@ const Signup = (props) => {
             <label htmlFor="Password" className="form-label">Password</label>
             <input type="password" className="form-control" onChange={onChange} id="password" name="password" minLength={5} required />
           </div>
-          <div className="mb-3">
+          {/* <div className="mb-3">
             <label htmlFor="cPassword1" className="form-label">Confirm Password</label>
             <input type="password" className="form-control" onChange={onChange} id="cpassword" name="cpassword" minLength={5} required />
-          </div>  
+          </div>   */}
           <button type="submit" className="btn btn-primary" >Submit</button>
         </form>
       </div>
